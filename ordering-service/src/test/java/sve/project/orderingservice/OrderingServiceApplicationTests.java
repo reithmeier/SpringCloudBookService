5d611ae04248cc6900f08392250b25fe9bce10c6
Lukas Reithmeier <lukas.reithmeier@fh-hagenberg.at>
2019-06-20 16:27:33 +0200

    added OrderingServiceApplication

---
+++ ordering-service/src/test/java/sve/project/orderingservice/OrderingServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package sve.project.orderingservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderingServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
