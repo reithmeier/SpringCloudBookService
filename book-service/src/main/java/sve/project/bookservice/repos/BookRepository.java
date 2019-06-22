a254fc1843bc49a580e887722a2f67a3b14d1166
Lukas Reithmeier <lukas.reithmeier@fh-hagenberg.at>
2019-06-22 13:44:26 +0200

    fixed rating service eureka settings

--- book-service/src/main/java/sve/project/bookservice/repos/BookRepository.java
+++ book-service/src/main/java/sve/project/bookservice/repos/BookRepository.java
@@ -4,16 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import sve.project.bookservice.domain.Book;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
-  /*  List<Book> findAll();
 
-    Optional<Book> findById(String isbn);
-
-    Book save(Book entity);
-
-    long count();
-
-    void delete(Book entity);
-
-    boolean existsById(String isbn);
-*/
 }
--- rating-service/src/main/java/sve/project/ratingservice/RatingServiceApplication.java
+++ rating-service/src/main/java/sve/project/ratingservice/RatingServiceApplication.java
@@ -3,6 +3,7 @@ package sve.project.ratingservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import sve.project.ratingservice.domain.Book;
 import sve.project.ratingservice.domain.Rating;
@@ -11,6 +12,7 @@ import sve.project.ratingservice.service.BookService;
 import sve.project.ratingservice.service.RatingService;
 import sve.project.ratingservice.service.UserService;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RatingServiceApplication {
 
